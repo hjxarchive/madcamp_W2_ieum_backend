ec6096d2f431d7a3f6c5a1c310c4590202b85331
hjxarchive <hanjinx102@gmail.com>
2026-01-17 16:46:20 +0900

    Setup project structure and User API

---
+++ src/main/kotlin/controller/HealthController.kt
@@ -0,0 +1,13 @@
+package com.example.ieum.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/api/health")
+    fun health(): String {
+        return "이음(ieum) 서버가 정상적으로 응답하고 있습니다!"
+    }
+}
